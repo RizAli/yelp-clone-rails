5fd25315150a632a8537c120864499a3ea4200ef
Tom Coakes <tomcoakes@gmail.com>
2015-04-01 15:59:41 +0100

    Review object created

---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'reviews' do
+  before {Restaurant.create name: 'KFC'}
+  
+  scenario 'leaving a review' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "Choked on some gristle..."
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('Choked on some gristle...')
+  end 
+end
