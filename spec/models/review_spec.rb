e92ce5d11e10af3aa021b965def54ffc7b726f92
Tom Coakes <tomcoakes@gmail.com>
2015-04-02 16:50:06 +0100

    Added a unit test to check that reviews are deleted along with restaurants

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   before do
+    @all_reviews = Review.all
     @kfc = Restaurant.create(name: "KFC")
     @kfc.reviews.create(thoughts: "Nice food", rating: "4")
   end
 
   it "can be assigned to a specific restaurant" do
     expect(@kfc.reviews[0].thoughts).to eq 'Nice food'
+  end
+
+  it "is also deleted when the associated restaurant is deleted" do
+    @kfc.destroy
+    expect(@all_reviews).not_to include "Nice food"
   end
 
 end
