d2b4fbf0c329cae26788d24764b48a4f7b1463ff
Tom Coakes <tomcoakes@gmail.com>
2015-04-03 18:11:06 +0100

    Secret tokens put into secrets.yml and accessed via that file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, '641796915953864', '1a7d51a0872da7080ba98c45c08931e0'
+  config.omniauth :facebook, "#{Rails.application.secrets.facebook_app_id}", "#{Rails.application.secrets.facebook_app_secret}"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
